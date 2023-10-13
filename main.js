c1bd48362b5cf6dce395c7c138bc66ca9d5d284a
Jlouisa <jonathan.louisa@gmail.com>
2023-10-13 20:12:32 +0200

    Add working version calculator

--- main.js
+++ main.js
@@ -15,11 +15,36 @@ const app = (() => {
         multiply(num1, num2) {
             return num1 * num2;
         }
+        root(num1) {
+            const num2 = 1;
+            return Math.sqrt(num1 * num2);
+        }
     }
     const calculator = new Calc();
-    //! History Arrays
+    class History {
+        constructor(historyNum1, methods, historyNum2, result) {
+            this.historyNum1 = historyNum1;
+            this.methods = ` ${methods} `;
+            this.historyNum2 = historyNum2;
+            this.result = result;
+        }
+    }
+    class History2 {
+        constructor(historyNum1, methods, result) {
+            this.historyNum1 = historyNum1;
+            this.methods = methods;
+            this.result = result;
+        }
+    }
+    //! History and input Arrays
     let inputArr = [];
+    let calcMethod = "";
     let calcArr = [];
+    let historyArr = [];
+    let smallScreenArr = [];
+    //! Falsy and Truthy
+    let equalState = false;
+    let numState = false;
     //! Caching DOM
     const buttonsEl = document.querySelector(".buttonsLayout");
     const screenEl = document.querySelector(".screen");
@@ -29,7 +54,7 @@ const app = (() => {
     bigScreenEl.classList.add("bigScreen");
     bigScreenEl.textContent = "0";
     const smallScreenEl = document.createElement("div");
-    smallScreenEl.textContent = "1";
+    smallScreenEl.textContent = "0";
     smallScreenEl.classList.add("smallScreen");
     screenEl === null || screenEl === void 0 ? void 0 : screenEl.appendChild(bigScreenEl);
     screenEl === null || screenEl === void 0 ? void 0 : screenEl.appendChild(smallScreenEl);
@@ -38,7 +63,7 @@ const app = (() => {
         "C",
         "CE",
         "⌫",
-        "X",
+        "×",
         7,
         8,
         9,
@@ -73,19 +98,138 @@ const app = (() => {
             i === 14 ||
             i === 16 ||
             i === 17) {
-            //! Add eventListener
-            arr[i].addEventListener("click", function () {
-                console.log(contentArr[i]);
-                inputArr.push(contentArr[i]);
-                console.log(inputArr);
+            //! Add eventListener to numbers
+            arr[i].addEventListener("click", () => {
+                numState = true;
+                if (equalState === false) {
+                    showSmallScreen(contentArr[i]);
+                    console.log(contentArr[i]);
+                    inputArr.push(contentArr[i]);
+                }
+                if (equalState === true) {
+                    inputArr = [];
+                    //   smallScreenArr = [];
+                    showSmallScreen(contentArr[i]);
+                    console.log(contentArr[i]);
+                    inputArr.push(contentArr[i]);
+                    equalState = false;
+                }
+            });
+        }
+        if (i === 3 || i === 7 || i === 11 || i === 15 || i === 18) {
+            //! Add eventListener to methods
+            arr[i].addEventListener("click", () => {
+                if (numState) {
+                    if (inputArr.length === 0 && calcArr.length !== 0) {
+                        calcArr.pop();
+                        smallScreenArr.pop();
+                        showSmallScreen(` ${contentArr[i]} `);
+                        calcMethod = contentArr[i];
+                        calcArr.push(calcMethod);
+                        console.log(contentArr[i]);
+                        console.log(calcArr);
+                    }
+                    else {
+                        console.log(contentArr[i]);
+                        showSmallScreen(` ${contentArr[i]} `);
+                        calcArr.push(Number(inputArr.join("")));
+                        calcArr.push(contentArr[i]);
+                        inputArr = [];
+                        console.log(calcArr);
+                        calcMethod = contentArr[i];
+                        console.log(calcMethod);
+                    }
+                }
             });
         }
     }
-    arr[11].addEventListener("click", () => {
-        console.log(contentArr[11], "testing");
-        calcArr.push(inputArr.join(""));
-        calcArr.push(contentArr[11]);
-        inputArr = [];
+    // Equal
+    arr[19].addEventListener("click", () => {
+        equalState = true;
+        calcArr.push(Number(inputArr.join("")));
         console.log(calcArr);
+        calculation(calcArr[0], calcMethod[0], calcArr[2]);
     });
+    // Clear
+    arr[0].addEventListener("click", () => {
+        inputArr = [];
+        smallScreenArr = [];
+        calcMethod = "";
+        bigScreenEl.textContent = "0";
+        console.log("cleared");
+    });
+    // Clear Everything
+    arr[1].addEventListener("click", () => {
+        inputArr = [];
+        calcArr = [];
+        smallScreenArr = [];
+        calcMethod = "";
+        equalState = false;
+        numState = false;
+        bigScreenEl.textContent = "0";
+        smallScreenEl.textContent = "0";
+        console.log("Cleared Everything");
+    });
+    // Delete
+    arr[2].addEventListener("click", () => {
+        inputArr.pop();
+        showSmallScreenDel();
+        console.log(inputArr);
+    });
+    //! Calculation Controller Module
+    function calculation(num1, theMethod, num2) {
+        switch (theMethod) {
+            case "+": {
+                executeCalc(num1, num2, theMethod, calculator.add);
+                break;
+            }
+            case "-": {
+                executeCalc(num1, num2, theMethod, calculator.sub);
+                break;
+            }
+            case "×": {
+                executeCalc(num1, num2, theMethod, calculator.multiply);
+                break;
+            }
+            case "÷": {
+                executeCalc(num1, num2, theMethod, calculator.divide);
+                break;
+            }
+            case "√": {
+                executeCalc2(num1, theMethod, calculator.root);
+                break;
+            }
+            default: {
+                console.log("nothing");
+                break;
+            }
+        }
+    }
+    function executeCalc(num1, num2, theMethod, func) {
+        const result = func(num1, num2);
+        historyArr.push(new History(num1, theMethod, num2, result));
+        bigScreenEl.textContent = result.toString();
+        calcArr = [];
+        inputArr = result.toString().split(",");
+        console.log(historyArr);
+    }
+    function executeCalc2(num1, theMethod, func) {
+        const result = func(num1);
+        historyArr.push(new History2(num1, theMethod, result));
+        bigScreenEl.textContent = result.toString();
+        calcArr = [];
+        inputArr = result.toString().split(",");
+        console.log(historyArr);
+    }
+    function showSmallScreen(element) {
+        smallScreenArr.push(element);
+        smallScreenEl.textContent = smallScreenArr.join("");
+    }
+    function showSmallScreenDel() {
+        smallScreenArr.pop();
+        if (smallScreenArr.length === 0) {
+            smallScreenArr[0] = 0;
+        }
+        smallScreenEl.textContent = smallScreenArr.join("");
+    }
 })();
--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ const app = (() => {
     sub(num1: number, num2: number): number;
     divide(num1: number, num2: number): number;
     multiply(num1: number, num2: number): number;
+    multiply(num1: number, num2: number): number;
+    root(num1: number, num2: number): number;
   }
 
   //! Calculator class
@@ -20,12 +22,61 @@ const app = (() => {
     multiply(num1: number, num2: number): number {
       return num1 * num2;
     }
+    root(num1: number): number {
+      const num2: number = 1;
+      return Math.sqrt(num1 * num2);
+    }
   }
   const calculator = new Calc();
 
-  //! History Arrays
+  //! Create class history
+  interface theHistory {
+    historyNum1: number;
+    methods: string;
+    historyNum2: number;
+  }
+
+  class History implements theHistory {
+    historyNum1: number;
+    methods: string;
+    historyNum2: number;
+    result: number;
+
+    constructor(historyNum1: number, methods: string, historyNum2: number, result: number) {
+      this.historyNum1 = historyNum1;
+      this.methods = ` ${methods} `;
+      this.historyNum2 = historyNum2;
+      this.result = result;
+    }
+  }
+
+  interface theHistory2 {
+    historyNum1: number;
+    methods: string;
+  }
+
+  class History2 implements theHistory2 {
+    historyNum1: number;
+    methods: string;
+    result: number;
+
+    constructor(historyNum1: number, methods: string, result: number) {
+      this.historyNum1 = historyNum1;
+      this.methods = methods;
+      this.result = result;
+    }
+  }
+
+  //! History and input Arrays
   let inputArr: (number | string)[] = [];
+  let calcMethod: string = "";
   let calcArr: (number | string)[] = [];
+  let historyArr: object[] = [];
+  let smallScreenArr: (number | string)[] = [];
+
+  //! Falsy and Truthy
+  let equalState = false;
+  let numState = false;
 
   //! Caching DOM
   const buttonsEl: HTMLElement | null = document.querySelector(".buttonsLayout");
@@ -37,7 +88,7 @@ const app = (() => {
   bigScreenEl.classList.add("bigScreen");
   bigScreenEl.textContent = "0";
   const smallScreenEl: HTMLElement | null = document.createElement("div");
-  smallScreenEl.textContent = "1";
+  smallScreenEl.textContent = "0";
   smallScreenEl.classList.add("smallScreen");
   screenEl?.appendChild(bigScreenEl);
   screenEl?.appendChild(smallScreenEl);
@@ -47,7 +98,7 @@ const app = (() => {
     "C",
     "CE",
     "⌫",
-    "X",
+    "×",
     7,
     8,
     9,
@@ -84,19 +135,143 @@ const app = (() => {
       i === 16 ||
       i === 17
     ) {
-      //! Add eventListener
-      arr[i].addEventListener("click", function () {
-        console.log(contentArr[i]);
-        inputArr.push(contentArr[i]);
-        console.log(inputArr);
+      //! Add eventListener to numbers
+      arr[i].addEventListener("click", () => {
+        numState = true;
+        if (equalState === false) {
+          showSmallScreen(contentArr[i]);
+          console.log(contentArr[i]);
+          inputArr.push(contentArr[i]);
+        }
+        if (equalState === true) {
+          inputArr = [];
+          //   smallScreenArr = [];
+          showSmallScreen(contentArr[i]);
+          console.log(contentArr[i]);
+          inputArr.push(contentArr[i]);
+          equalState = false;
+        }
+      });
+    }
+    if (i === 3 || i === 7 || i === 11 || i === 15 || i === 18) {
+      //! Add eventListener to methods
+      arr[i].addEventListener("click", () => {
+        if (numState) {
+          if (inputArr.length === 0 && calcArr.length !== 0) {
+            calcArr.pop();
+            smallScreenArr.pop();
+            showSmallScreen(` ${contentArr[i]} `);
+            calcMethod = contentArr[i] as string;
+            calcArr.push(calcMethod);
+            console.log(contentArr[i]);
+            console.log(calcArr);
+          } else {
+            console.log(contentArr[i]);
+            showSmallScreen(` ${contentArr[i]} `);
+            calcArr.push(Number(inputArr.join("")));
+            calcArr.push(contentArr[i] as string);
+            inputArr = [];
+            console.log(calcArr);
+            calcMethod = contentArr[i] as string;
+            console.log(calcMethod);
+          }
+        }
       });
     }
   }
-  arr[11].addEventListener("click", () => {
-    console.log(contentArr[11], "testing");
-    calcArr.push(inputArr.join(""));
-    calcArr.push(contentArr[11]);
-    inputArr = [];
+
+  // Equal
+  arr[19].addEventListener("click", () => {
+    equalState = true;
+    calcArr.push(Number(inputArr.join("")));
     console.log(calcArr);
+    calculation(calcArr[0] as number, calcMethod[0] as string, calcArr[2] as number);
   });
+  // Clear
+  arr[0].addEventListener("click", () => {
+    inputArr = [];
+    smallScreenArr = [];
+    calcMethod = "";
+    bigScreenEl.textContent = "0";
+    console.log("cleared");
+  });
+  // Clear Everything
+  arr[1].addEventListener("click", () => {
+    inputArr = [];
+    calcArr = [];
+    smallScreenArr = [];
+    calcMethod = "";
+    equalState = false;
+    numState = false;
+    bigScreenEl.textContent = "0";
+    smallScreenEl.textContent = "0";
+    console.log("Cleared Everything");
+  });
+  // Delete
+  arr[2].addEventListener("click", () => {
+    inputArr.pop();
+    showSmallScreenDel();
+    console.log(inputArr);
+  });
+
+  //! Calculation Controller Module
+  function calculation(num1: number, theMethod: string, num2: number): void {
+    switch (theMethod) {
+      case "+": {
+        executeCalc(num1, num2, theMethod, calculator.add);
+        break;
+      }
+      case "-": {
+        executeCalc(num1, num2, theMethod, calculator.sub);
+        break;
+      }
+      case "×": {
+        executeCalc(num1, num2, theMethod, calculator.multiply);
+        break;
+      }
+      case "÷": {
+        executeCalc(num1, num2, theMethod, calculator.divide);
+        break;
+      }
+      case "√": {
+        executeCalc2(num1, theMethod, calculator.root);
+        break;
+      }
+      default: {
+        console.log("nothing");
+        break;
+      }
+    }
+  }
+
+  function executeCalc(num1: number, num2: number, theMethod: string, func: Function) {
+    const result = func(num1, num2);
+    historyArr.push(new History(num1, theMethod, num2, result));
+    bigScreenEl!.textContent = result.toString();
+    calcArr = [];
+    inputArr = result.toString().split(",");
+    console.log(historyArr);
+  }
+
+  function executeCalc2(num1: number, theMethod: string, func: Function) {
+    const result = func(num1);
+    historyArr.push(new History2(num1, theMethod, result));
+    bigScreenEl!.textContent = result.toString();
+    calcArr = [];
+    inputArr = result.toString().split(",");
+    console.log(historyArr);
+  }
+
+  function showSmallScreen(element: number | string): void {
+    smallScreenArr.push(element);
+    smallScreenEl!.textContent = smallScreenArr.join("");
+  }
+
+  function showSmallScreenDel() {
+    smallScreenArr.pop();
+    if (smallScreenArr.length === 0) {
+      smallScreenArr[0] = 0;
+    }
+    smallScreenEl!.textContent = smallScreenArr.join("");
+  }
 })();
